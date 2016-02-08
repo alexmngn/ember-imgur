5768fbfb99391bb993d5709ee1d154900e735ab4
Alexis Mangin <me@alexismangin.com>
2016-02-08 16:20:31 -0500

    Remove update/delete methods

--- tests/dummy/app/controllers/application.js
+++ tests/dummy/app/controllers/application.js
@@ -12,16 +12,6 @@ export default Ember.Controller.extend({
 
 	imageGetResult: null,
 
-	imageUpdateValue: null,
-
-	imageUpdateTitleValue: null,
-
-	imageUpdateResult: null,
-
-	imageDeleteValue: null,
-
-	imageDeleteResult: null,
-
 	actions: {
 		imagePost() {
 			const fileToLoad = Ember.$('#image-post')[0].files[0];
@@ -43,22 +33,6 @@ export default Ember.Controller.extend({
 				this.set('imageGetResult', 'Success! '+result.data.link+' <br><img src="'+result.data.link+'">');
 			}).catch((result) => {
 				this.set('imageGetResult', 'Error '+result.data.error);
-			});
-		},
-
-		imageUpdate() {
-			this.get('imgur').imageUpdate(this.get('imageUpdateValue'), this.get('imageUpdateTitleValue')).then((result) => {
-				this.set('imageUpdateResult', 'Success! '+result.data);
-			}).catch((result) => {
-				this.set('imageUpdateResult', 'Error '+result.data.error);
-			});
-		},
-
-		imageDelete() {
-			this.get('imgur').imageDelete(this.get('imageDeleteValue')).then((result) => {
-				this.set('imageDeleteResult', 'Success! '+result.data);
-			}).catch((result) => {
-				this.set('imageDeleteResult', 'Error '+result.data.error);
 			});
 		}
 	}
